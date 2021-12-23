7da504631f6bd4c9713696681303c1c5e2742c46
Elamir-Nady <ElamirNady@gmail.com>
2021-12-23 02:33:16 +0200

    Athuntecation-Athurization DotNet 5

---
+++ Athuntecation-Athurization/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using AthuntecationAthurization.Models;
+using System.Threading.Tasks;
+
+namespace AthuntecationAthurization.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+    }
+}
